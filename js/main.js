ceffc5ceb5cafb5e87365af00728aa5b2c6674c4
luAr26101 <raul.cosmin.26@icloud.com>
2025-07-01 08:03:36 +0300

    Complete mobile navigation.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
 import { data } from "../data/data.js";
 
 console.log(data);
+
+const mobileNavBtn = document.querySelector(".mobile-nav-toggle");
+const primaryNavigation = document.querySelector("#primary-navigation");
+mobileNavBtn.addEventListener("click", function () {
+  const visibility = primaryNavigation.getAttribute("data-visible");
+  console.log(typeof visibility);
+  if (visibility === "false") {
+    primaryNavigation.setAttribute("data-visible", true);
+    mobileNavBtn.setAttribute("aria-expanded", true);
+  } else {
+    primaryNavigation.setAttribute("data-visible", false);
+    mobileNavBtn.setAttribute("aria-expanded", false);
+  }
+});
